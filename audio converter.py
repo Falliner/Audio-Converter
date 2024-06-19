81da0ba005a9493acffa3b70ecd6dda971107de8
Falliner <142126722+Falliner@users.noreply.github.com>
2024-06-18 19:55:45 -0600

    Update audio converter.py

--- audio converter.py
+++ audio converter.py
@@ -7,7 +7,7 @@ output_file_name = input("Enter the name of the output audio file (with extensio
 input_file = os.path.join(os.path.expanduser("~"), "Downloads", input_file_name)
 output_file = os.path.join(os.path.expanduser("~"), "Downloads", output_file_name)
 
-# Extract audio from the video using FFmpeg
+#Extract audio from the video using FFmpeg
 command = [
     "ffmpeg",
     "-ss", "00:00:00", 
